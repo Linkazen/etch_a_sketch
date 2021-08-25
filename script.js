6d36296eba14d3aafeb0c95f9367059b09394a7e
Linkazen <jpch2k4@gmail.com>
2021-08-25 21:10:22 +0100

    final changes

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const parentGridDiv = document.querySelector('#parentGrid')
+const clearBtn = document.querySelector('.clear')
+let size = document.querySelector('#sizeChoice')
+let gridDiv = null
+let numGridDivs = null
+let rows = 0
+let cols = 0
 
-// for every column and row create a div in the html that
-// is part of a grid
+
+
+function switchClass() {
+    this.classList.add("changeColour")
+}
+
+// creates the grid using rows and columns as an argument which is defined in func setsize
+// then appends the children to the 
 
 function createGrid(rows, cols) {
     parentGridDiv.style.setProperty('--grid-rows', rows)
@@ -12,7 +24,65 @@ function createGrid(rows, cols) {
     }
 }
 
-createGrid(16, 16)
+// adds an event listener to all the griddivs
+
+function mouseOverGridsAdd() {
+    for (arrayNum = 0; arrayNum < numGridDivs; arrayNum++) {
+        gridDiv[arrayNum].addEventListener('mouseover', switchClass)
+    } 
+}
+
+function clearBoard() {
+    for (arrayNum2 = 0; arrayNum2 < numGridDivs; arrayNum2++) {
+        gridDiv[arrayNum2].classList.remove("changeColour")
+        gridDiv[arrayNum2].classList.add("gridSlot")
+    }
+}
+
+function setSize() {
+    if(size.selectedIndex == 0) {
+        rows = 4
+        cols = 4
+    } else if(size.selectedIndex == 1) {
+        rows = 8
+        cols = 8
+    } else if(size.selectedIndex == 2) {
+        rows = 16
+        cols = 16
+    } else if(size.selectedIndex == 3) {
+        rows = 32
+        cols = 32
+    } else if(size.selectedIndex == 4) {
+        rows = 64
+        cols = 64
+    } else if(size.selectedIndex == 5) {
+        rows = 100
+        cols = 100
+    } else {
+        console.log("error")
+        return
+    }
+    
+    createGrid(rows, cols)
+    
+    gridDiv = document.querySelectorAll('.gridSlot')
+    numGridDivs = gridDiv.length
+    
+    mouseOverGridsAdd()
+}
 
 
 
+clearBtn.onclick = function() {
+    clearBoard()
+}
+
+size.addEventListener("click", function() {
+    clearBoard()
+    for(penis = 0; penis < numGridDivs; penis++) {
+        gridDiv[penis].parentNode.removeChild(gridDiv[penis])
+    }
+    setSize()
+})
+
+setSize()
